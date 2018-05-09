a456464ed76e20146aa253767c1ea4a0966c8f2a
Hume <daniel.hume@hp.com>
2018-05-09 16:28:57 -0600

    Added necessary modules and provider to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatInputModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { PainterService } from './painter.service';
+import { MatCalendar } from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule, 
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [PainterService],
+  bootstrap: [AppComponent],
+  entryComponents: [MatCalendar]
 })
 export class AppModule { }
