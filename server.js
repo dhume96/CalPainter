0caafd085a674babaf150b2f51e1edcdf4ec22fe
Hume <daniel.hume@hp.com>
2018-05-09 17:33:22 -0600

    Added the correct path for the static files

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/CalPainter'));
 
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, function() {
+    console.log(`Express server listeing in port ${process.env.PORT || 8080} you can access the webapp on http://localhost:8080/`);
+});
